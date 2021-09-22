71d3060d36c1793c1178d517f791eb9a01653bd8
Darren Jaworski <darrenjaworski@gmail.com>
2021-09-21 19:02:52 -0500

    Handle date range.

--- oscn.mjs
+++ oscn.mjs
@@ -2,18 +2,59 @@ import cheerio from "cheerio";
 import fs from "fs";
 import https from "https";
 import fetch from "node-fetch";
+import pkg from "date-fns";
+
+const { format, addDays, compareDesc } = pkg;
 
 class OSCNScraper {
   baseURL = "https://www.oscn.net/applications/oscn/";
-  URL =
-    "report.asp?report=WebJudicialDocketEventAll&errorcheck=true&database=&db=Oklahoma&EventCode=72440&StartDate=01%2F02%2F2019&GeneralNumber=1&generalnumber1=1";
+  startDate = new Date(2020, 0, 1); // month is zero indexed, start of the year;
+  endDate = new Date();
+  evictionText = RegExp("FORCIBLE ENTRY & DETAINER");
 
   main = async () => {
-    const url = `${this.baseURL}${this.URL}`;
-    const reportPage = await this.getPage(url);
+    let searchDate = this.startDate;
+    // loop through dates
+    while (compareDesc(searchDate, this.endDate) > 0) {
+      const url = this.getSearchUrl(searchDate);
+      console.log(url);
+      const reportPage = await this.getPage(url);
 
-    const $ = cheerio.load(reportPage);
+      console.log(`searching for cases on ${format(searchDate, "MM/dd/yyyy")}`);
+      await this.searchDayReportPage(
+        reportPage,
+        format(searchDate, "MM-dd-yyyy")
+      );
+
+      searchDate = addDays(searchDate, 1);
+    }
+  };
+
+  getPage = async (url) => {
+    let body = "";
+    let response = undefined;
+
+    try {
+      response = await fetch(url);
+      body = await response.text();
+
+      return body;
+    } catch (error) {
+      console.log(error);
+    }
+
+    return body;
+  };
+
+  getSearchUrl = (date) => {
+    const urlEncodedDate = encodeURI(format(date, "MM/dd/yyyy"));
+    return `${this.baseURL}report.asp?report=WebJudicialDocketEventAll&errorcheck=true&database=&db=Oklahoma&EventCode=72440&StartDate=${urlEncodedDate}&GeneralNumber=1&generalnumber1=1`;
+  };
+
+  searchDayReportPage = async (pageContent, dateString) => {
+    const $ = cheerio.load(pageContent);
     let links = [];
+
     $(".clspg a").each((i, e) => {
       links.push($(e).attr("href"));
     });
@@ -22,8 +63,25 @@ class OSCNScraper {
       const link = links[index];
 
       const linkPage = await this.getPage(`${this.baseURL}${link}`);
-      console.log(`checking page ${link} for files`);
       const $link = cheerio.load(linkPage);
+
+      let hasForcibleEntry = false;
+
+      const textTags = $link(".docketEntry font > font");
+
+      console.log(`searching page ${link} for evictions`);
+      for (let index = 0; index < textTags.length; index++) {
+        const tag = textTags[index];
+        const text = $(tag).text();
+        hasForcibleEntry = this.evictionText.test(text);
+
+        if (hasForcibleEntry) break;
+      }
+
+      if (!hasForcibleEntry) break;
+
+      console.log(`${link} has an eviction`);
+      console.log("searching page for files");
 
       $link(".docketEntry a").each((i, e) => {
         const imageUrl = $(e).attr("href");
@@ -31,11 +89,13 @@ class OSCNScraper {
         const barcode = queryParams[queryParams.length - 1].split("=")[1];
         const caseNumber = queryParams[queryParams.length - 3].split("=")[1];
         const fileLocation = `${this.baseURL}${imageUrl}`;
-        console.log(`grabbing file from ${fileLocation}`);
-        const file = fs.createWriteStream(
-          `./files/${caseNumber}-${barcode}.tif`
-        );
+
         try {
+          console.log(`grabbing file from ${fileLocation}`);
+          const file = fs.createWriteStream(
+            `./files/${dateString}-${caseNumber}-${barcode}.tif`
+          );
+
           https.get(fileLocation, (res) => {
             res.pipe(file);
           });
@@ -44,12 +104,6 @@ class OSCNScraper {
         }
       });
     }
-  };
-
-  getPage = async (url) => {
-    const response = await fetch(url);
-    const body = await response.text();
-    return body;
   };
 }
 
--- readme.md
+++ readme.md
@@ -1,7 +1,16 @@
 # getting started
 
+Node LTS is recommended.
+
 ```bash
 $ npm install
 
 $ npm start
+```
+
+To clean the files:
+```bash
+$ npm run clean
+
+$ npm install
 ```
